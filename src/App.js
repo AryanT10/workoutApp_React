bd5169c7fa4e3c7943cdc54b4ec54292c5a1e4d5
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-08-12 22:53:48 -0400

    FEAT: navbar.jsx added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+import ExerciseDetail from './components/pages/ExerciseDetail'
+import { Home } from '@mui/icons-material'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-	<div>App</div>
+    <Box width="400px" sx={{width:{xl:'1448px'}}}m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" elements={<Home />} />
+        <Route path="/" elements={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+
   )
 }
 
