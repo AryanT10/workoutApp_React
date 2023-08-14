90f9275fa814c5e4d41760380f1ecd68f42d4d87
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-08-13 21:12:40 -0400

    FEAT: solve render error and add SearchExercises.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { Box } from '@mui/material'
-import Home from './components/pages/Home'
-import ExerciseDetail from './components/pages/ExerciseDetail'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const App = () => {
-  return (
-    <Box width="400px" sx={{width:{xl:'1448px'}}}m="auto">
-      <Navbar />
-      <Routes>
-        <Route path="/" elements={<Home />} />
-        <Route path="/" elements={<ExerciseDetail />} />
-      </Routes>
-      <Footer />
-    </Box>
+import './App.css';
+import ExerciseDetail from './components/pages/ExerciseDetail';
+import Home from './components/pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-  )
-}
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
-export default App
+export default App;
