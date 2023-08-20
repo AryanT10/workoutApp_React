b34928e3b0f1b817f308840407892d645dc156f6
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-08-20 13:23:44 -0400

    FEAT: Check on the component HorizontalScrollBar.js

--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { ScrollMenu,VisibilityContext } from 'react-horizontal-scrolling-menu';
-
 import BodyPart from './BodyPart';
 const HorizontalScrollBar = ({ data,bodyPart,setBodyPart }) => {
 	return (
 		<ScrollMenu>
 			{data.map((item) => (
-
 				<Box
 					key={item.id || item}
 					itemId={item.id || item}
@@ -16,8 +14,7 @@ const HorizontalScrollBar = ({ data,bodyPart,setBodyPart }) => {
 				>
 					<BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
 				</Box>
-			)
-			)}
+			))}
 		</ScrollMenu>
 	)
 }
